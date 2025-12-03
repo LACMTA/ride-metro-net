10dbeefd1db684b7aedf7880e47789f49d413198
Parker Woodworth <pswoodworth@gmail.com>
2025-12-02 16:24:09 -0800

    adding trip index import on build

--- src/integrations/import-gtfs.ts
+++ src/integrations/import-gtfs.ts
@@ -1,4 +1,9 @@
-import { importGtfs } from "gtfs";
+import { importGtfs, openDb } from "gtfs";
+import fs from "fs";
+import path from "path";
+
+const TRIPS_DIR = "./data";
+const TRIPS_FILE = "trips.json";
 
 export const GTFSconfig = {
   sqlitePath: ":memory:",
@@ -21,7 +26,31 @@ export default function importGTFS() {
       "astro:config:done": async () => {
         console.log("Importing GTFS to in-memory SQLite database...");
         await importGtfs(GTFSconfig);
+        getTripIndex();
       },
     },
   };
 }
+
+const SEPERATOR = ",";
+
+const getTripIndex = async function () {
+  const db = openDb(GTFSconfig);
+  const query = ` 
+    SELECT 
+      trips.route_id,
+      GROUP_CONCAT(trips.trip_id, '${SEPERATOR}') as trip_ids
+    FROM trips 
+    LEFT JOIN routes ON routes.route_id = trips.route_id
+	  GROUP BY routes.route_id
+        `;
+  const results = (await db.prepare(query).all()) as [
+    { route_id: string; trip_ids: string },
+  ];
+  const trips = results.map((trip) => ({
+    [trip.route_id]: trip.trip_ids.split(SEPERATOR),
+  }));
+
+  fs.mkdirSync(TRIPS_DIR, { recursive: true });
+  fs.writeFileSync(path.join(TRIPS_DIR, TRIPS_FILE), JSON.stringify(trips));
+};
