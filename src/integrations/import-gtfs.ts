54cfabf7b155c0d5f81b5e6d52194c33cf84fd0b
Parker Woodworth <pswoodworth@gmail.com>
2025-12-22 17:00:53 -0500

    removing schedule experiment

--- src/integrations/import-gtfs.ts
+++ src/integrations/import-gtfs.ts
@@ -1,9 +1,4 @@
 import { importGtfs, openDb } from "gtfs";
-import fs from "fs";
-import path from "path";
-
-const TRIPS_DIR = "./data";
-const TRIPS_FILE = "trips.json";
 
 export const GTFSconfig = {
   sqlitePath: ":memory:",
@@ -30,35 +25,7 @@ export default function importGTFS() {
       "astro:config:done": async () => {
         console.log("Importing GTFS to in-memory SQLite database...");
         await importGtfs(GTFSconfig);
-        getTripIndex();
       },
     },
   };
 }
-
-const SEPERATOR = ",";
-
-const getTripIndex = async function () {
-  const db = openDb(GTFSconfig);
-  const query = ` 
-    SELECT 
-      trips.route_id,
-      GROUP_CONCAT(trips.trip_id, '${SEPERATOR}') as trip_ids
-    FROM trips 
-    LEFT JOIN routes ON routes.route_id = trips.route_id
-	  GROUP BY routes.route_id
-        `;
-  const results = (await db.prepare(query).all()) as [
-    { route_id: string; trip_ids: string },
-  ];
-  const trips = results.reduce(
-    (res, trip) => {
-      res[trip.route_id] = trip.trip_ids.split(SEPERATOR);
-      return res;
-    },
-    {} as Record<string, string[]>,
-  );
-
-  fs.mkdirSync(TRIPS_DIR, { recursive: true });
-  fs.writeFileSync(path.join(TRIPS_DIR, TRIPS_FILE), JSON.stringify(trips));
-};
