38c2040b8a20a11d443c85da98c38aabe1598c8b
Parker Woodworth <pswoodworth@gmail.com>
2026-01-15 15:26:22 -0800

    styling progress

--- src/components/BusIcon.tsx
+++ src/components/BusIcon.tsx
@@ -6,8 +6,8 @@ export default function MetroIcon({ className = "text-metro-text" }: Props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="30"
-      height="32"
+      viewBox="0 0 30 32"
+      preserveAspectRatio="xMinYMin"
       fill="none"
       className={className}
     >
---
+++ src/components/BusPill.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  name: string;
+}
+
+export default function BusPill({ name }: Props) {
+  return (
+    <span className="text-background-white bg-bus-local rounded-lg px-2.5 py-0.5 font-bold">
+      {name}
+    </span>
+  );
+}
