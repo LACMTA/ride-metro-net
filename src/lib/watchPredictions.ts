288f703325e1e9993ab1a130a50e18056f03ae52
Parker Woodworth <pswoodworth@gmail.com>
2026-01-28 16:41:35 -0800

    auto-prediction handling

--- src/lib/watchPredictions.ts
+++ src/lib/watchPredictions.ts
@@ -11,11 +11,38 @@ async function getPredictions(stopId: string) {
   routePredictions.set(data);
 }
 
+function adjustPredictions(secondsDelta: number) {
+  const allPredictions = routePredictions.get();
+  const newPredictions = allPredictions.map((p) => {
+    return Object.assign(p, {
+      destinations: p.destinations.map((d) => {
+        return Object.assign(d, {
+          predictions: d.predictions.map((prediction) => {
+            return Object.assign(prediction, {
+              time: prediction.time + secondsDelta,
+              sec: prediction.sec - secondsDelta,
+              min: Math.floor((prediction.sec - secondsDelta) / 60),
+            });
+          }),
+        });
+      }),
+    });
+  });
+  console.log("Updated predictions", newPredictions);
+  routePredictions.set(newPredictions);
+}
+
 export default async function watchPredictions(
   stopId: string,
-  pollInterval: number = 15000,
+  pollInterval: number = 60000,
+  adjustmentInterval: number = 10000,
 ) {
   const curriedGetPredictions = getPredictions.bind(null, stopId);
+  const curriedadjustPredictions = adjustPredictions.bind(
+    null,
+    adjustmentInterval / 1000,
+  );
   curriedGetPredictions();
+  setInterval(curriedadjustPredictions, adjustmentInterval);
   return setInterval(curriedGetPredictions, pollInterval);
 }
