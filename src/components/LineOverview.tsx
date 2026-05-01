75467298f9a4df4d9fcb47f939c1c8f7709d32b6
Parker Woodworth <pswoodworth@gmail.com>
2026-05-01 13:45:35 -0700

    overview improvements

---
+++ src/components/LineOverview.tsx
@@ -0,0 +1,80 @@
+import MapPinIcon from "./MapPinIcon";
+import ClockIcon from "./ClockIcon";
+import DownloadIcon from "./DownloadIcon";
+import Button from "./Button";
+import { formatGTFSTime } from "../lib/formatGTFSTime";
+import type { RouteOverview } from "../lib/getRouteOverview";
+
+interface Props {
+  lineTitle: string;
+  routeOverview: RouteOverview;
+  pdfUrl: string;
+  routeType: number;
+}
+
+export default function LineOverview({
+  lineTitle,
+  routeOverview,
+  pdfUrl,
+  routeType,
+}: Props) {
+  const pluralVehicleName = routeType === 3 ? "Buses" : "Trains";
+  return (
+    <div className="pt-5 pb-50">
+      <h2 className="mb-6 text-2xl font-bold">{lineTitle} Overview</h2>
+      <div className="flex">
+        <MapPinIcon aria-hidden="true" className="mr-3 inline h-6 w-6" />
+        <p>
+          {pluralVehicleName} run from{" "}
+          <a
+            href={`/stops/${routeOverview.firstStopId}`}
+            className="text-blue underline"
+          >
+            {routeOverview.firstStopName}
+          </a>
+          {" to "}
+          <a
+            href={`/stops/${routeOverview.lastStopId}`}
+            className="text-blue underline"
+          >
+            {routeOverview.lastStopName}
+          </a>{" "}
+          with {routeOverview.stopCount} stops between.
+        </p>
+      </div>
+      {routeOverview.weekday.first &&
+        routeOverview.weekday.last &&
+        routeOverview.weekend.first &&
+        routeOverview.weekend.last && (
+          <div className="mt-6 mb-10 flex">
+            <ClockIcon aria-hidden="true" className="mr-3 inline h-6 w-6" />
+            <div>
+              <p>
+                <b>Monday through Friday</b>
+                <br />
+                {pluralVehicleName} run between{" "}
+                {formatGTFSTime(routeOverview.weekday.first)} and{" "}
+                {formatGTFSTime(routeOverview.weekday.last)}
+              </p>
+              <p className="mt-1">
+                <b>Saturdays, Sundays, and Holidays</b>
+                <br />
+                {pluralVehicleName} run between{" "}
+                {formatGTFSTime(routeOverview.weekend.first)} and{" "}
+                {formatGTFSTime(routeOverview.weekend.last)}
+              </p>
+            </div>
+          </div>
+        )}
+
+      <h2 className="mb-4 text-2xl font-bold">{lineTitle} Details</h2>
+      <p className="mb-4">
+        Find stop times, maps, and more in the document below.
+      </p>
+      <Button as="a" href={pdfUrl} target="_blank" rel="noopener noreferrer">
+        View Schedule and Map{" "}
+        <DownloadIcon className="inline text-white" aria-hidden="true" />
+      </Button>
+    </div>
+  );
+}
--- src/components/LineTabs.tsx
+++ src/components/LineTabs.tsx
@@ -4,19 +4,16 @@ import { useStore } from "@nanostores/react";
 import Column from "./Column";
 import { StyledTab, StyledTabList, StyledTabPanelWrapper } from "./StyledTabs";
 import AlertsSection from "./AlertsSection";
+import LineOverview from "./LineOverview";
 import { alerts } from "../lib/alertsStore";
-import Button from "./Button";
-import DownloadIcon from "./DownloadIcon";
 import type { RouteOverview } from "../lib/getRouteOverview";
-import MapPinIcon from "./MapPinIcon";
-import ClockIcon from "./ClockIcon";
-import { formatGTFSTime } from "../lib/formatGTFSTime";
 
 interface Props {
   routeId: string;
   lineTitle: string;
   routeOverview: RouteOverview;
   pdfUrl: string;
+  routeType: number;
 }
 
 export default function LineTabs({
@@ -24,6 +21,7 @@ export default function LineTabs({
   lineTitle,
   pdfUrl,
   routeOverview,
+  routeType,
 }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const $alerts = useStore(alerts);
@@ -65,78 +63,7 @@ export default function LineTabs({
         <Column>
           <TabPanels>
             <TabPanel>
-              <div className="pt-5 pb-50">
-                <h2 className="mb-6 text-2xl font-bold">
-                  {lineTitle} Overview
-                </h2>
-                <div className="flex">
-                  <MapPinIcon
-                    aria-hidden="true"
-                    className="mr-3 inline h-6 w-6"
-                  />
-                  <p>
-                    Busses run from{" "}
-                    <a
-                      href={`/stops/${routeOverview.firstStopId}`}
-                      className="text-blue underline"
-                    >
-                      {routeOverview.firstStopName}
-                    </a>
-                    {" to "}
-                    <a
-                      href={`/stops/${routeOverview.lastStopId}`}
-                      className="text-blue underline"
-                    >
-                      {routeOverview.lastStopName}
-                    </a>{" "}
-                    with {routeOverview.stopCount} stops between.
-                  </p>
-                </div>
-                {routeOverview.weekday.first &&
-                  routeOverview.weekday.last &&
-                  routeOverview.weekend.first &&
-                  routeOverview.weekend.last && (
-                    <div className="mt-6 mb-10 flex">
-                      <ClockIcon
-                        aria-hidden="true"
-                        className="mr-3 inline h-6 w-6"
-                      />
-                      <div>
-                        <p>
-                          <b>Monday through Friday</b>
-                          <br />
-                          Busses run between{" "}
-                          {formatGTFSTime(routeOverview.weekday.first)} and{" "}
-                          {formatGTFSTime(routeOverview.weekday.last)}
-                        </p>
-                        <p className="mt-1">
-                          <b>Saturdays, Sundays, and Holidays</b>
-                          <br />
-                          Busses run between{" "}
-                          {formatGTFSTime(routeOverview.weekday.first)} and{" "}
-                          {formatGTFSTime(routeOverview.weekday.last)}
-                        </p>
-                      </div>
-                    </div>
-                  )}
-
-                <h2 className="mb-4 text-2xl font-bold">{lineTitle} Details</h2>
-                <p className="mb-4">
-                  Find stop times, maps, and more in the document below.
-                </p>
-                <Button
-                  as="a"
-                  href={pdfUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Schedule and Map{" "}
-                  <DownloadIcon
-                    className="inline text-white"
-                    aria-hidden="true"
-                  />
-                </Button>
-              </div>
+              <LineOverview lineTitle={lineTitle} routeOverview={routeOverview} pdfUrl={pdfUrl} routeType={routeType} />
             </TabPanel>
             <TabPanel>
               <AlertsSection routeId={routeId} />
