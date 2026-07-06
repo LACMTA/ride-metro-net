503d887b9f34e8f21889849bd494588c876dfe13
Parker Woodworth <pswoodworth@gmail.com>
2026-07-06 14:48:09 -0700

    attempt 2 at avoiding bots

--- src/lib/agencies.ts
+++ src/lib/agencies.ts
@@ -194,10 +194,16 @@ export const agencyConfigs: AgencyConfig[] = [
       fillEmptyAgencyId: true,
       prefix: "torrance-transit-",
       url: "https://transit.torranceca.gov/gtfs_feed",
-      // Needed to avoid bot blockers
+      // Needed to avoid bot detectors
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
       },
       realtimeAlerts: {
         url: "http://www.mybusinfo.com/gtfsrt/alerts",
