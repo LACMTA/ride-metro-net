c5f3176c7b4b637900819c310e70abe72c4dccee
Parker Woodworth <pswoodworth@gmail.com>
2026-09-02 10:31:29 -0700

    adding link to metro.net to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,9 @@ export default function Header() {
   return (
     <header className="flex h-16 items-center bg-black">
       <Column wide>
-        <MetroLockup className="text-background-white inline h-10 shrink-0 grow-0" />
+        <a href="https://www.metro.net/">
+          <MetroLockup className="text-background-white inline h-10 shrink-0 grow-0" />
+        </a>
       </Column>
     </header>
   );
